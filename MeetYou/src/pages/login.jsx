e06e26105148208ed334119e4bfed785e33b04fa
DarkKevo <kevinaraujogonzalez@gmail.com>
2023-02-09 22:49:52 -0400

    Login Listo (CSS sin Responsive)

--- MeetYou/src/pages/login.jsx
+++ MeetYou/src/pages/login.jsx
@@ -1,11 +1,25 @@
-import React from 'react'
+import React from 'react';
+import '../css/LoginStyles.css';
 
 export function Login() {
   return (
-    <div>
-        <nav>
-            
-        </nav>
+    <div className='login-container'>
+      <nav>
+        <img src='./src/assets/meetYou.png' />
+      </nav>
+      <div className='form-container'>
+        <form>
+          <div className='container-inputs'>
+            <h2>Iniciar Sesion</h2>
+            <input type='text' placeholder='Usuario' />
+            <input type='password' placeholder='Contraseña' />
+          </div>
+          <div className='container-buttons'>
+            <button id='create'>Crear Cuenta</button>
+            <button id='login'>Iniciar Sesion</button>
+          </div>
+        </form>
+      </div>
     </div>
-  )
+  );
 }
